a3d03b0a5b2e788b06fde1930e8ea356c33de654
PriyaG88 <priya.g71488@gmail.com>
2018-03-08 11:54:09 -0800

    Refactor session form from a class compononet to a functional component

--- query/frontend/components/session/session_form.jsx
+++ query/frontend/components/session/session_form.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
 
-class SessionForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const SessionForm = props => {
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.loggedIn) {
-      this.props.history.push('/');
-    }
-  }
-
-  render() {
-    return (
-      <div className="background-container">
-        <div className="session-form-container">
-          <h1 className='main-logo'>Query</h1>
-          <h5 className='tagline'>A place to find answers for those burning questions</h5>
-          <div className="login-form-container">
-            <LoginFormContainer/>
-          </div>
-          <div className="signup-form-container">
-            <SignupFormContainer/>
-          </div>
+  return (
+    <div className="background-container">
+      <div className="session-form-container">
+        <h1 className='main-logo'>Query</h1>
+        <h5 className='tagline'>A place to find answers for those burning questions</h5>
+        <div className="login-form-container">
+          <LoginFormContainer/>
+        </div>
+        <div className="signup-form-container">
+          <SignupFormContainer/>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withRouter(SessionForm);
+
+export default SessionForm;
