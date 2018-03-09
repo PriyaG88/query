0b93bc958f7ef80cd252f62a33f61312324de1cb
PriyaG88 <priya.g71488@gmail.com>
2018-03-09 12:24:07 -0800

    Add component lifecycle method to cause rerender when new answer is added

--- query/frontend/components/answer/answer_index.jsx
+++ query/frontend/components/answer/answer_index.jsx
@@ -4,15 +4,28 @@ import AnswerIndexItem from './answer_index_item';
 class AnswerIndex extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   componentDidMount() {
     this.props.fetchAnswers();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.filteredAnswers(this.props).length !== this.filteredAnswers(nextProps).length) {
+      this.props.fetchAnswers();
+    }
+  }
+
+  filteredAnswers(props) {
+    return (
+      props.answers.filter(
+        answer => answer.question_id === this.props.question.id )
+    );
+  }
+
   render() {
-    const filteredAnswers = this.props.answers.filter(answer => answer.question_id === this.props.question.id);
+    const filteredAnswers = this.filteredAnswers(this.props);
+
     return (
       <div>
         <div className="answers-count">{filteredAnswers.length} Answer(s)</div>
