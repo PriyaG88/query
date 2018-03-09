f2018989f5614d3ccb5aac5f045ec8713d1f6e87
PriyaG88 <priya.g71488@gmail.com>
2018-03-09 11:48:38 -0800

    Refactor to functional component

--- query/frontend/components/answer/answer_index_item.jsx
+++ query/frontend/components/answer/answer_index_item.jsx
@@ -3,33 +3,26 @@ import CommentFormContainer from '../comment/comment_form_container';
 import CommentIndexContainer from '../comment/comment_index_container';
 import Avatar from 'react-avatar';
 
-class AnswerIndexItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.answer = this.props.answer;
+const AnswerIndexItem = props =>  {
 
-  }
+  const answer = props.answer;
+  const author = props.answer.author.name;
 
-
-  render() {
-    const author = this.props.answer.author.name;
-
-    return (
-      <div className="answer-item-index">
-        <div className="avatar">
-          <Avatar name={author} size={40} round={true} textSizeRatio={2} />
-        </div>
-        <div className="asker-name">{author}</div>
-        {this.answer.body}
-        <div className="comment-section">
-          <div className="comment-form"><CommentFormContainer id={this.answer.id}/></div>
-        </div>
-        <div>
-          <div className="comment-index"><CommentIndexContainer answer={this.answer}/></div>
-        </div>
+  return (
+    <div className="answer-item-index">
+      <div className="avatar">
+        <Avatar name={author} size={40} round={true} textSizeRatio={2} />
       </div>
-    );
-  }
-}
+      <div className="asker-name">{author}</div>
+      {answer.body}
+      <div className="comment-section">
+        <div className="comment-form"><CommentFormContainer id={answer.id}/></div>
+      </div>
+      <div>
+        <div className="comment-index"><CommentIndexContainer answer={answer}/></div>
+      </div>
+    </div>
+  );
+};
 
 export default AnswerIndexItem;
