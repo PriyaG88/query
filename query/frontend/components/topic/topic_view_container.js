da1c210702728b8ea75abe6585ec4bd0604349a5
PriyaG88 <priya.g71488@gmail.com>
2018-03-11 14:19:55 -0700

    Add createAnswer to props

--- query/frontend/components/topic/topic_view_container.js
+++ query/frontend/components/topic/topic_view_container.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { questions, topics, answers } from '../../reducers/selectors';
 import { fetchQuestions } from '../../actions/question_actions';
 import { fetchTopics } from '../../actions/topic_actions';
-import { fetchAnswers } from '../../actions/answer_actions';
+import { fetchAnswers, createAnswer } from '../../actions/answer_actions';
 import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = (state, ownProps) => ({
@@ -17,7 +17,8 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = dispatch => ({
   fetchQuestions: () => dispatch(fetchQuestions()),
   fetchTopics: () => dispatch(fetchTopics()),
-  fetchAnswers: () => dispatch(fetchAnswers())
+  fetchAnswers: () => dispatch(fetchAnswers()),
+  createAnswer: answer => dispatch(createAnswer(answer))
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TopicView));
