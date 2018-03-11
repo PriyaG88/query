f8d8200e29785026dd072a292b77272f338434fd
PriyaG88 <priya.g71488@gmail.com>
2018-03-11 14:52:23 -0700

    Refactor homepage to a functional component

--- query/frontend/components/home_page.jsx
+++ query/frontend/components/home_page.jsx
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import QuestionIndexContainer from './question/question_index_container';
 import TopicIndexContainer from './topic/topic_index_container';
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className='homepage-container'>
-        <TopicIndexContainer />
-        <QuestionIndexContainer />
-      </div>
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <div className='homepage-container'>
+      <TopicIndexContainer />
+      <QuestionIndexContainer />
+    </div>
+  );
+};
 
 export default HomePage;
