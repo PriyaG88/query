f933af415f18a887ae86c77605e9cc906396bec2
PriyaG88 <priya.g71488@gmail.com>
2018-03-13 18:41:22 -0700

    Restructure jsx to mirror presentation of question_index_item

--- query/frontend/components/answer/answer_index_item.jsx
+++ query/frontend/components/answer/answer_index_item.jsx
@@ -7,14 +7,29 @@ import Avatar from 'react-avatar';
 const AnswerIndexItem = props =>  {
   const answer = props.answer;
   const author = answer.author.name;
+  const date = new Date(Date.parse(props.question.created_at));
 
   return (
     <div className="answer-item-index">
-      <div className="avatar">
-        <Avatar name={author} size={40} round={true} textSizeRatio={2} />
+      <div className="answer-section">
+        <div className="answer-user-info">
+          <div className="avatar">
+            <Avatar className="answer-avatar"
+              name={author}
+              size={40}
+              round={true}
+              textSizeRatio={2} />
+            <div className="answer-details">
+              <div className="name">{answer.author.name}</div>
+              <div className="date-answered">Answered on {date.toDateString()}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="question-answer-item">
+          {ReactHtmlParser(answer.body)}
+        </div>
       </div>
-      <div className="asker-name">{author}</div>
-      {ReactHtmlParser(answer.body)}
       <div className="comment-section">
         <div className="comment-form">
           <CommentFormContainer id={answer.id}/>
