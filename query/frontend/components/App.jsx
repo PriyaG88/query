8436f264375a8a6feaff3cb29ead3c1780834257
PriyaG88 <priya.g71488@gmail.com>
2018-03-13 23:02:01 -0700

    Remove unused SessionFormContainer and replace reference in App.jsx

--- query/frontend/components/App.jsx
+++ query/frontend/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import SessionFormContainer from './session/session_form_container';
+import SessionForm from './session/session_form';
 import NavBarContainer from './navbar/navbar_container';
 import QuestionViewContainer from './question/question_view_container';
 import TopicViewContainer from './topic/topic_view_container';
@@ -10,7 +10,7 @@ import QuestionsToAnswerContainer from './question/questions_to_answer_container
 
 const App = () => (
   <div>
-    <AuthRoute path="/enter" component={SessionFormContainer} />
+    <AuthRoute path="/enter" component={SessionForm} />
     <ProtectedRoute path="/" component={NavBarContainer} />
     <Switch>
       <ProtectedRoute path="/topics/:id" component={TopicViewContainer} />
