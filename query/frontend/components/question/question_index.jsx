09437400e2d0fa22295a87a35aa87387a60bf507
PriyaG88 <priya.g71488@gmail.com>
2018-03-15 20:34:17 -0700

    Update component state to include answers

--- query/frontend/components/question/question_index.jsx
+++ query/frontend/components/question/question_index.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import QuestionIndexItem from './question_index_item';
 import Avatar from 'react-avatar';
+import QuestionIndexItem from './question_index_item';
 import QuestionModal from './question_modal';
+import Loader from '../ui/Loader';
 
 class QuestionIndex extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      answers: []
+    };
   }
 
   componentDidMount() {
     this.props.fetchQuestions();
-    this.props.fetchAnswers();
+    this.props.fetchAnswers()
+    .then(answers => this.setState({ answers: Object.values(answers.answers) }));
     this.props.fetchComments();
   }
 
@@ -18,28 +23,37 @@ class QuestionIndex extends Component {
     if (this.props.questions.length !== nextProps.questions.length ) {
       this.props.fetchQuestions();
     } else if (this.props.answers.length !== nextProps.answers.length ) {
-      this.props.fetchAnswers();
+      this.props.fetchAnswers()
+      .then(answers => this.setState({ answers: Object.values(answers.answers) }));
     }
   }
 
   render() {
-    const questions = this.props.questions.reverse().map(question => (
-      <QuestionIndexItem
-        key={question.id}
-        question={question}
-        answers={this.props.answers}
-        comments={this.props.comments}
-        currentUser={this.props.currentUser}
-        updateQuestion={this.props.updateQuestion}
-        deleteQuestion={this.props.deleteQuestion}
-        createAnswer={this.props.createAnswer} />
-    ));
+    if (this.state.answers.length > 0) {
+      const questions = this.props.questions.reverse().map(question => (
+        <QuestionIndexItem
+          key={question.id}
+          question={question}
+          answers={this.state.answers}
+          comments={this.props.comments}
+          currentUser={this.props.currentUser}
+          updateQuestion={this.props.updateQuestion}
+          deleteQuestion={this.props.deleteQuestion}
+          createAnswer={this.props.createAnswer} />
+      ));
+
+      return (
+        <ul className="question-index">
+          <QuestionModal currentUser={this.props.currentUser}/>
+          {questions}
+        </ul>
+      );
+    }
+
     return (
-      <ul className="question-index">
-        <QuestionModal currentUser={this.props.currentUser}/>
-        {questions}
-      </ul>
+      <Loader />
     );
+
   }
 }
 
