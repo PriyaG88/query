e9e799836362f354fc5f2953cd3df09c3bacb869
PriyaG88 <priya.g71488@gmail.com>
2018-03-15 20:36:20 -0700

    Update answers method to return null if state hasn't updated and render a loading icon

--- query/frontend/components/question/question_index_item.jsx
+++ query/frontend/components/question/question_index_item.jsx
@@ -5,6 +5,7 @@ import AnswerEditor from '../answer/answer_editor';
 import Avatar from 'react-avatar';
 import CommentFormContainer from '../comment/comment_form_container';
 import CommentIndexContainer from '../comment/comment_index_container';
+import Loader from '../ui/Loader';
 
 class QuestionIndexItem extends React.Component {
   constructor(props) {
@@ -24,14 +25,15 @@ class QuestionIndexItem extends React.Component {
     this.setState({editorIsOpen: !this.state.editorIsOpen});
   }
 
-  answers() {
+  answer() {
     const id = this.props.question.id;
-    let answers = this.props.answers.filter(answer => answer.question_id === id);
-    return answers[0];
+    const answers = this.props.answers.filter(answer => answer.question_id === id);
+    const answer = answers.length > 0 ? answers[0] : null;
+    return answer;
   }
 
   render() {
-    const answer = this.answers();
+    const answer = this.answer();
     const currentUser = this.props.currentUser;
     if (!answer) {
       return (
@@ -60,45 +62,53 @@ class QuestionIndexItem extends React.Component {
       );
     }
 
-    let date = new Date(Date.parse(answer.created_at));
-    return (
-      <div className="question-item-box">
-        <div className="question-item">
-          <div className="question-text-container">
-            <h6 className="question-header">Answer · {this.props.question.topic}</h6>
-            <Link className="question-item-link" to={`/questions/${this.props.question.id}`}>
-              <span className="question">{this.props.question.body}</span>
-            </Link>
-          </div>
-          <div className="answer-section">
-            <div className="answer-user-info">
-              <div className="avatar">
-                <Avatar className="answer-avatar"
-                  name={answer.author.name}
-                  size={40}
-                  round={true}
-                  textSizeRatio={2} />
-                <div className="answer-details">
-                  <div className="name">{answer.author.name}</div>
-                  <div className="date-answered">Answered on {date.toDateString()}
+    else if (answer.author) {
+      const date = new Date(Date.parse(answer.created_at));
+      return (
+        <div className="question-item-box">
+          <div className="question-item">
+            <div className="question-text-container">
+              <h6 className="question-header">Answer · {this.props.question.topic}</h6>
+              <Link className="question-item-link" to={`/questions/${this.props.question.id}`}>
+                <span className="question">{this.props.question.body}</span>
+              </Link>
+            </div>
+            <div className="answer-section">
+              <div className="answer-user-info">
+                <div className="avatar">
+                  <Avatar className="answer-avatar"
+                    name={answer.author.name}
+                    size={40}
+                    round={true}
+                    textSizeRatio={2} />
+                  <div className="answer-details">
+                    <div className="name">{answer.author.name}</div>
+                    <div className="date-answered">Answered on {date.toDateString()}
+                    </div>
                   </div>
-                </div>
 
+                </div>
+              </div>
+              <div className="question-answer-item">
+                {ReactHtmlParser(answer.body)}
               </div>
             </div>
-            <div className="question-answer-item">
-              {ReactHtmlParser(answer.body)}
+            <div className="comment-section">
+              <CommentFormContainer id={answer.id}/>
+            </div>
+            <div className="comment-index">
+              <div className="comment-index-container"><CommentIndexContainer answer={answer}/></div>
             </div>
           </div>
-          <div className="comment-section">
-            <CommentFormContainer id={answer.id}/>
-          </div>
-          <div className="comment-index">
-            <div className="comment-index-container"><CommentIndexContainer answer={answer}/></div>
-          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    else {
+      return (
+        <Loader />
+      );
+    }
   }
 }
 
