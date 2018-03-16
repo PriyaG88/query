97465dfa9f3019fd7871e8b55d6c48e71292c9c0
PriyaG88 <priya.g71488@gmail.com>
2018-03-15 20:40:28 -0700

    Update state to include answers and render loading icon if answers have not yet been retrieved

--- query/frontend/components/answer/answer_index.jsx
+++ query/frontend/components/answer/answer_index.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
 import AnswerIndexItem from './answer_index_item';
+import Loader from '../ui/Loader';
 
 class AnswerIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      answers: []
+    };
   }
 
   componentDidMount() {
-    this.props.fetchAnswers();
+    this.props.fetchAnswers()
+    .then(answers => this.setState({ answers: Object.values(answers.answers) }));
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.filteredAnswers(this.props).length !== this.filteredAnswers(nextProps).length) {
-      this.props.fetchAnswers();
+    if (this.props.answers.length !== nextProps.answers.length) {
+      this.props.fetchAnswers()
+      .then(answers => this.setState({ answers: Object.values(answers.answers) }));
     }
   }
 
-  filteredAnswers(props) {
+  filteredAnswers() {
     return (
-      props.answers.filter(
+      this.state.answers.filter(
         answer => answer.question_id === this.props.question.id )
     );
   }
 
   render() {
-    const filteredAnswers = this.filteredAnswers(this.props);
+    if (this.state.answers.length > 0) {
+      const filteredAnswers = this.filteredAnswers();
 
+      return (
+        <div>
+          <div className="answers-count">{filteredAnswers.length} Answer(s)</div>
+          <ul className="answer-index">
+            {filteredAnswers.map(answer => (
+              <AnswerIndexItem
+                key={answer.id}
+                answer={answer}
+                question={this.props.question}
+                fetchUser={this.props.fetchUser}
+                currentUser={this.props.currentUser}
+                deleteAnswer={this.props.deleteAnswer}/>
+            ))}
+          </ul>
+        </div>
+      );
+    }
     return (
-      <div>
-        <div className="answers-count">{filteredAnswers.length} Answer(s)</div>
-        <ul className="answer-index">
-          {filteredAnswers.map(answer => (
-            <AnswerIndexItem
-              key={answer.id}
-              answer={answer}
-              question={this.props.question}
-              fetchUser={this.props.fetchUser}
-              currentUser={this.props.currentUser}
-              deleteAnswer={this.props.deleteAnswer}/>
-          ))}
-        </ul>
-      </div>
+      <Loader />
     );
   }
 }
