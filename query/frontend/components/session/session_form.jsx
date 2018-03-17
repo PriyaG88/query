8405f6c2bcd5a0c5982d807e410d79597b301739
PriyaG88 <priya.g71488@gmail.com>
2018-03-16 18:43:37 -0700

    Update component to a class component to handle local state for toggling signup form

--- query/frontend/components/session/session_form.jsx
+++ query/frontend/components/session/session_form.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoginFormContainer from './login_form_container';
 import SignupFormContainer from './signup_form_container';
 
-const SessionForm = props => {
+class SessionForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSignup: false
+    };
+    this.toggleSignup = this.toggleSignup.bind(this);
+  }
 
-  return (
-    <div className='background-container'>
-      <div className="session-form-container">
-        <h1 className='main-logo'>Query</h1>
-        <h5 className='tagline'>A place to find answers for those burning questions</h5>
-        <div className="login-form-container">
-          <LoginFormContainer/>
-        </div>
-        <div className="signup-form-container">
-          <SignupFormContainer/>
+  toggleSignup() {
+    this.setState(prevState => {
+      return { showSignup: !prevState.showSignup };
+    });
+  }
+
+  render() {
+    return (
+      <div className='background-container'>
+        <div className="session-form-container">
+          <h1 className='main-logo'>Query</h1>
+          <h5 className='tagline'>A place to find answers for those burning questions</h5>
+          <div className="form-wrapper">
+            <div className='signup-form-container'>
+              <SignupFormContainer
+                toggleSignup={this.toggleSignup}
+                showSignup={this.state.showSignup}/>
+            </div>
+            <div className="login-form-container">
+              <LoginFormContainer/>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 
 export default SessionForm;
