b3a29d8cde9add9e74a3fa32c008de6ab0a952eb
PriyaG88 <priya.g71488@gmail.com>
2018-03-18 15:31:58 -0700

    Remove unused actions

--- query/frontend/components/question/question_index_container.js
+++ query/frontend/components/question/question_index_container.js
@@ -1,16 +1,12 @@
 import { connect } from 'react-redux';
-import {
-  fetchQuestions,
-  updateQuestion,
-  deleteQuestion
-  } from '../../actions/question_actions';
+import { fetchQuestions } from '../../actions/question_actions';
 import { fetchAnswers, createAnswer } from '../../actions/answer_actions';
 import { fetchComments } from '../../actions/comment_actions';
 import { answers, questions, comments } from '../../reducers/selectors';
 import QuestionIndex from './question_index';
 
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = state => ({
   questions: questions(state),
   answers: answers(state),
   comments: comments(state),
@@ -21,8 +17,6 @@ const mapDispatchToProps = dispatch => ({
   fetchQuestions: () => dispatch(fetchQuestions()),
   fetchAnswers: () => dispatch(fetchAnswers()),
   fetchComments: () => dispatch(fetchComments()),
-  updateQuestion: question => dispatch(updateQuestion(question)),
-  deleteQuestion: question => dispatch(deleteQuestion(question)),
   createAnswer: answer => dispatch(createAnswer(answer))
 });
 
